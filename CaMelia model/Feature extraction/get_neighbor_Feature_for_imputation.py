ef5c364681758b9e8083586839d145144e9ba1f1
JxTang-bioinformatics <55084319+JxTang-bioinformatics@users.noreply.github.com>
2020-12-16 21:21:51 +0800

    Update get_neighbor_Feature_for_imputation.py

--- CaMelia model/Feature extraction/get_neighbor_Feature_for_imputation.py
+++ CaMelia model/Feature extraction/get_neighbor_Feature_for_imputation.py
@@ -19,6 +19,37 @@ from concurrent.futures import ProcessPoolExecutor,as_completed
 import warnings
 warnings.filterwarnings('ignore')
 ##############################################################
+def reduce_mem(df):
+    #starttime = time.time()
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    #start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if pd.isnull(c_min) or pd.isnull(c_max):
+                continue
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    #end_mem = df.memory_usage().sum() / 1024**2
+    #print('-- Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction),time spend:{:2.2f} min'.format(end_mem,                                                                                                          100*(start_mem-end_mem)/start_mem,                                                                                                           (time.time()-starttime)/60))
+    return df
+
 @jit
 def chu(a,n):
     return round(a/(2*n),4)
@@ -48,6 +79,7 @@ def unionfile(file_dir,meragefiledir,filenames,filenames1,i1):
         for i in range(len(filenames)):
             path = r'%s/%s/%s' %(meragefiledir,filenames[i],filenames1[j])
             data = pd.read_csv(path,header=0,sep='\t')
+
             if i==0:
                 df = pd.merge(df,data,how='outer')
             else:
@@ -55,7 +87,7 @@ def unionfile(file_dir,meragefiledir,filenames,filenames1,i1):
             #print (j,i,len(df))
         df = df.drop_duplicates(['chrom','location'])
         df[list(df)[2:]] = df[list(df)[2:]].astype('float16')
-        df[list(df)[2:]] = df[list(df)[2:]].round(4)
+        df[list(df)[2:]] = df[list(df)[2:]].round(4)       
         df.to_csv(r'%s/%s' % (file_dir,filenames1[j]),sep='\t',header=True,index=False)     
     return ('jincheng%d:Done!' % i1)
 
@@ -164,6 +196,7 @@ if __name__ == '__main__':
 
     path = r'%s/%s' % (gse,ff)
     data = pd.read_csv(path,header=0,sep='\t')
+    data = reduce_mem(data)
     if list(data)[0] != 'chrom':
         del data['%s' % list(data)[0]]
     
