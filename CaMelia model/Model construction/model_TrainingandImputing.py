dfa35dac183cd5bbc06cb64fa40164cd7565391e
JxTang-bioinformatics <55084319+JxTang-bioinformatics@users.noreply.github.com>
2020-12-16 21:23:44 +0800

    Update model_TrainingandImputing.py

--- CaMelia model/Model construction/model_TrainingandImputing.py
+++ CaMelia model/Model construction/model_TrainingandImputing.py
@@ -8,7 +8,7 @@ from __future__ import division
 from sys import argv
 import pandas as pd
 import numpy as np
-import os
+import os,time
 
 from numba import jit
 
@@ -22,7 +22,39 @@ import catboost as cb
 
 import warnings
 warnings.filterwarnings('ignore')
-
+##################################
+def reduce_mem(df):
+    starttime = time.time()
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    start_mem = df.memory_usage().sum() / 1024**2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if pd.isnull(c_min) or pd.isnull(c_max):
+                continue
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('-- Mem. usage decreased to {:5.2f} Mb ({:.1f}% reduction),time spend:{:2.2f} min'.format(end_mem,
+                                                                                                           100*(start_mem-end_mem)/start_mem,
+                                                                                                           (time.time()-starttime)/60))
+    return df
 ##################################
 @jit
 def auc(m, train, test):
@@ -106,12 +138,14 @@ if __name__ == '__main__':
         #Train data
         path = r'%s/%s' % (meragefiledir_out,filenames_out[i])
         dataset_train = pd.read_csv(path,header=0,sep='\t')
+        dataset_train = reduce_mem(dataset_train)
         dataset_train = dataset_train.dropna(axis=1,how='all')   
         dataset_train = dataset_train.dropna(axis=0,how='any')
         
         #Imputation data
         path = r'%s/%s' % (meragefiledir_impu,filenames_out[i])
         dataset_impu = pd.read_csv(path,header=0,sep='\t')
+        dataset_impu = reduce_mem(dataset_impu)        
         dataset_impu = dataset_impu.dropna(axis=1,how='all')   
         dataset_impu = dataset_impu.dropna(axis=0,how='any') 
     
